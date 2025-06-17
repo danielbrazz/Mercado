72befc95245bf5ba318c9eeea4af05b6bc9cc22d
Daniel Braz <daniel.brazz02@gmail.com>
2025-06-17 17:03:59 -0300

    Adicionado nova função para aparecer a mensagem de alerta. Essa mesma mensagem aparecer no canto inferior direito da tela  e após 2 segundos vai sumir

--- Mercado/frontend/js/messageAlert.js
+++ Mercado/frontend/js/messageAlert.js
@@ -1,17 +1,23 @@
-function trata_alerta(tipo,msg){
-//tipo 1 alerta
-//tipo 2 success
 
-switch(tipo){
-    case 1:
-        $("#alert").show();
-        $("#alert").text(msg);
-    break;
-    case 2:
-        $("#success").show();
-        $("#success").text(msg);
-    break
-}
+function trata_alerta(tipo, msg) {
+    let alert;
 
+    switch (tipo) {
+        case 1:
+            alert = $("#alert");
+            break;
+        case 2:
+            alert = $("#success");
+            break;
+    }
 
+    if (alert) {
+        
+        alert.text(msg).fadeIn();//animação para aparecer gradualmente
+
+     
+        setTimeout(() => {
+            alert.fadeOut();//some com animação em 2 segundos.
+        }, 2000);
+    }
 }
